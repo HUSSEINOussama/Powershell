203563372e0f8a88168b89a2ebf3bf3daa21bea7
HUSSEINOussama <32069384+HUSSEINOussama@users.noreply.github.com>
2017-09-19 15:11:27 +0200

    Update New-SUP.ps1

--- New-SUP.ps1
+++ New-SUP.ps1
@@ -2,11 +2,11 @@
 Param()
 Import-Module ($Env:SMS_ADMIN_UI_PATH.Substring(0,$Env:SMS_ADMIN_UI_PATH.Length-5) + '\ConfigurationManager.psd1')
 $NameDate = (Get-Date -Format "yyyy-MM")
-$SiteCode="FRM"
-$SiteServer="FRMSFICFMVP001"
+$SiteCode="***"
+$SiteServer="***"
 $UL=@()
 $UpdateList = @(Get-WmiObject -NameSpace root\SMS\Site_$($SiteCode) -ComputerName $SiteServer -Query "Select * from SMS_SoftwareUpdate Where IsExpired='false' And Severity='10' And LocalizedDisplayName NOT LIKE '%Vista%' AND DATEDIFF(day,DatePosted,GETDATE())<30")
-$UpdateList | Select LocalizedDisplayName,Severity,ArticleID,BulletinID,DatePosted,IsExpired,IsLatest | Export-Csv "MaJ Critiques $NameDate test oussama.csv" -Delimiter ';' -NoTypeInformation -Encoding Default
+$UpdateList | Select LocalizedDisplayName,Severity,ArticleID,BulletinID,DatePosted,IsExpired,IsLatest | Export-Csv "MaJ Critiques $NameDate.csv" -Delimiter ';' -NoTypeInformation -Encoding Default
 Foreach ($Update in $UpdateList)
 {
 $UpdateName = $Update.LocalizedDisplayName
@@ -14,4 +14,4 @@ Write-Verbose "$UpdateName a été ajouté au groupe de mises à jour logicielle
 $UL+=$Update.CI_ID
 }
 CD "$($SiteCode):"
-New-CMSoftwareUpdateGroup -Name " MaJ Critiques $NameDate test oussama" -UpdateId $UL 
+New-CMSoftwareUpdateGroup -Name " MaJ Critiques $NameDate" -UpdateId $UL 
